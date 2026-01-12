4f0bbe7ae4b04ff617f51f99ac588ca27f228896
Tawfiq H <tawfiq.hamid@gmail.com>
2026-01-12 22:56:53 +0000

    No more drawing cars before the player at spawn point -- cars drawn with negative y value have zero velocity

--- src/TreeManager.ts
+++ src/TreeManager.ts
@@ -26,27 +26,55 @@ export class EnvironmentObjectManager {
     // sortedEnvironmentObjectsY is a sorted array of environment objects by y-value
     // to check if they are within the view distance
     // updateEnvironmentObjectsInRange is a function to update the environment objects in the range
-    checkViewDistanceAndUpdateEnvironmentObjects(sortedEnvironmentObjectsY: Array<EnvironmentObject>, updateEnvironmentObjectsInRange: (minY: number, maxY: number, roadHalfWidth: number) => void): void {
-        // TBC - this could be a generic method for all environment objects (e.g. trees, vehicles, etc.)
-        const yUpperBound = this.gameState.player.y + this.viewDistance;
-        const yLowerBound = this.gameState.player.y - this.viewDistance;
+    checkViewDistanceAndUpdateEnvironmentObjects(
+        sortedEnvironmentObjectsY: Array<EnvironmentObject>,
+        updateEnvironmentObjectsInRange: (minY: number, maxY: number, roadHalfWidth: number) => void,
+        logger: string = ''
+    ): void {
+        // -- maxYwhereWeWantToDrawObjectsTo => upperBoundToGenerateTo
+        // 🔁 = we want to draw objects in this y-range
+        // ---- maxY = maxYwhereWeCurrentlyHaveObjectsUpTo
+        // 
+        // car
+        // 
+        // --- minY = minYwhereWeCurrentlyHaveObjectsFrom
+        //
+        // 🔁 = we want to draw objects in this y-range
+        //
+        // -- minYwhereWeWantToDrawObjectsFrom => lowerBoundToGenerateFrom
 
+
+        let maxYwhereWeWantToDrawObjectsTo = this.gameState.player.y + this.viewDistance;
+        let minYwhereWeWantToDrawObjectsFrom = this.gameState.player.y - this.viewDistance;
 
         // Find the actual min/max Y of remaining environment objects
-        const existingMinY = sortedEnvironmentObjectsY.length > 0 ? sortedEnvironmentObjectsY[0].y : this.gameState.player.y;
-        const existingMaxY = sortedEnvironmentObjectsY.length > 0 ? sortedEnvironmentObjectsY[sortedEnvironmentObjectsY.length - 1].y : this.gameState.player.y;
+        let minYwhereWeCurrentlyHaveObjectsFrom = sortedEnvironmentObjectsY.length > 0 ? sortedEnvironmentObjectsY[0].y : this.gameState.player.y;
+        let maxYwhereWeCurrentlyHaveObjectsUpTo = sortedEnvironmentObjectsY.length > 0 ? sortedEnvironmentObjectsY[sortedEnvironmentObjectsY.length - 1].y : this.gameState.player.y;
+
 
         const roadHalfWidth = this.calculateRoadWidth() / 2;
+
+        // When we generate objects it's expensive so we generate them quite far in advance so that we're not constantly regenerating new ones
+        // otherwise as soon as the player moves forward we'll generate new objects at the new view distance
+        const extraBufferToGenerateMoreObjects = this.viewDistance;
+
+
         // Generate environment objects ahead if needed
-        if (existingMaxY < yUpperBound) {
-            console.log('🌳🌳 generateEnvironmentObjects on Upper Bound', existingMaxY, yUpperBound);
-            updateEnvironmentObjectsInRange(existingMaxY, yUpperBound + this.viewDistance, roadHalfWidth);
+        if (maxYwhereWeWantToDrawObjectsTo > maxYwhereWeCurrentlyHaveObjectsUpTo) { // if there is a gap we generate objects ahead
+            const upperBoundToGenerateTo = maxYwhereWeWantToDrawObjectsTo + extraBufferToGenerateMoreObjects;
+
+            console.log(logger, 'generateEnvironmentObjects on Upper Bound', maxYwhereWeCurrentlyHaveObjectsUpTo, upperBoundToGenerateTo);
+
+            updateEnvironmentObjectsInRange(maxYwhereWeCurrentlyHaveObjectsUpTo, upperBoundToGenerateTo, roadHalfWidth);
         }
 
         // Generate environment objects behind if needed
-        if (existingMinY > yLowerBound) {
-            console.log('🌳🌳 generateEnvironmentObjects on lower Bound', yLowerBound, existingMinY);
-            updateEnvironmentObjectsInRange(yLowerBound - this.viewDistance, existingMinY, roadHalfWidth);
+        if (minYwhereWeCurrentlyHaveObjectsFrom > minYwhereWeWantToDrawObjectsFrom) {// if there is a gap we generate objects behind
+            let lowerBoundToGenerateFrom = minYwhereWeWantToDrawObjectsFrom - extraBufferToGenerateMoreObjects;
+
+            console.log(logger, 'generateEnvironmentObjects on lower Bound', `[${minYwhereWeWantToDrawObjectsFrom} .. ${minYwhereWeCurrentlyHaveObjectsFrom}]`, lowerBoundToGenerateFrom, minYwhereWeCurrentlyHaveObjectsFrom);
+
+            updateEnvironmentObjectsInRange(lowerBoundToGenerateFrom, minYwhereWeCurrentlyHaveObjectsFrom, roadHalfWidth);
         }
 
     }
@@ -65,14 +93,13 @@ export class TreesManager extends EnvironmentObjectManager {
 
     updateTrees(): void { //TBC this can be simplified if the player just moves forward
         const sortedTreesY = this.gameState.trees.sort((a, b) => a.y - b.y); // sort trees by y //TBC - don't sort -just keep the array sorted
-        this.checkViewDistanceAndUpdateEnvironmentObjects(sortedTreesY, this.generateTreesInRange.bind(this));
+        this.checkViewDistanceAndUpdateEnvironmentObjects(sortedTreesY, this.generateTreesInRange.bind(this), '🌳🌳');
     }
 
 
 
     generateTreesInRange(minY: number, maxY: number, roadHalfWidth: number): void {
         // console.log('🌳🌳 generateTreesInRange🌳', minY, maxY);
-
 
         // Left side
         for (let y = minY; y <= maxY; y += 1) {
--- src/VehiclesManager.ts
+++ src/VehiclesManager.ts
@@ -5,9 +5,8 @@ import { Vehicle } from './Vehicle.js';
 
 export class VehiclesManager extends EnvironmentObjectManager {
     // Constants =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
+    readonly minYForVehicles: number = 50;
     //trees
-    readonly carDensityPerYUnit: number = 1 / 20; // 1 car per 40 units of y-axis
     readonly baseCarSize: number = 1.5
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -22,17 +21,55 @@ export class VehiclesManager extends EnvironmentObjectManager {
         this.updateExistingVehicles(secondsSinceLastUpdate);
         const sortedVehiclesY = this.gameState.vehicles.sort((a, b) => a.y - b.y); // sort vehicles by y//TBC - don't sort -just keep the array sorted
 
-        this.checkViewDistanceAndUpdateEnvironmentObjects(sortedVehiclesY, this.generateVehiclesInRange.bind(this));
+        this.checkViewDistanceAndUpdateEnvironmentObjects(sortedVehiclesY, this.generateVehiclesInRange.bind(this), '🚗🚗');
 
     }
+
+
+    carDensityPerYUnit(y: number): number {
+        // Generate a vehicle at the minYForVehicles boundary
+        // AND then don't generate any more vehicles behind the player
+        // Otherwise the algorithm will keep trying to generate vehicles 2*viewDistance behind the player
+        // Avoid generating vehicles behind the player that immediately drive into the player
+        if (y == -this.minYForVehicles) {
+            return 1;
+        }
+        // Then don't generate any more vehicles behind the player
+
+
+        // if we're within the minYForVehicles range, we don't generate vehicles
+        // this is to avoid generating vehicles behind the player that immediately drive into the player
+        // also to avoid generating vehicles ahead of the player that the player drives into quickly
+        if (-this.minYForVehicles < y && y < this.minYForVehicles) {
+            return 0;
+        }
+
+        // y is the distance from the player
+        // we want to generate fewer vehicles as we get further away from the player
+        const startGeneratingVehiclesAtY = this.minYForVehicles;
+        const density = (-startGeneratingVehiclesAtY / y) + 1; // This is based on y=(-50/x) + 1
+
+        const maxDensity = 1 / 15;
+        return density * maxDensity; // reduce the density by 100x to make it more sparse
+
+    }
+
+    vehicleVelocityBasedOnY(y: number): number {
+        // vehicles get faster as we get further along the y-axis
+        if (y < 0) {
+            return 0;
+        }
+
+        return varyNumberByPercentage(15, 0.333); // Random velocity between 10 and 20
+    }
+
 
     generateVehiclesInRange(minY: number, maxY: number, roadHalfWidth: number): void {
         console.log('🚗🚗 generateVehiclesInRange🚗', minY, maxY);
 
-
         for (let y = minY; y <= maxY; y += 1) {
             for (let roadSide = -1; roadSide <= 1; roadSide += 2) { // -1 for left, 1 for right
-                if (Math.random() < this.carDensityPerYUnit) {
+                if (Math.random() < this.carDensityPerYUnit(y)) {
                     const randomOffset = Math.random() * roadHalfWidth;
                     const x = roadSide * (roadHalfWidth - randomOffset)
 
@@ -45,7 +82,7 @@ export class VehiclesManager extends EnvironmentObjectManager {
                         varyNumberByPercentage(length, 0.2),
                         randomShadeOfBlue(),
                         0,
-                        varyNumberByPercentage(15, 0.333) // Random velocity between 10 and 20
+                        this.vehicleVelocityBasedOnY(y)
                     );
 
                     this.gameState.vehicles.push(newVehicle);
